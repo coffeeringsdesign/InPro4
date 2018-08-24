79969a5320bd1c1ec40d12d420dd12ec0245a751
Kendra <kendra@coffeeringsdesign.com>
2018-08-24 12:24:37 -0700

    track where thread of logic is and mark place - lunch

--- js/scripts.js
+++ js/scripts.js
@@ -12,40 +12,20 @@ function Order(name, basePrice, toppings, toppingsPrice, totalPrice) {
 // empty array for toppings
 var pizzaToppings = [];
 
-// // object 1 - assigns price to small
-// var size1 = {
-//   name: "small",
-//   price: 7.00,
-//   topping: []
-// };
-//
-// // object2 - assigns price to medium
-// var size2 = {
-//   name: "medium",
-//   price: 10.00,
-//   topping: []
-// };
-//
-// // object3 - assigns price to large
-// var size3 = {
-//   name: "large",
-//   price: 15.00,
-//   topping: []
-// };
-Order.prototype.getsize = function(name) {
-  alert("function is being called");
-  if (this.name === "smallPizza") {
-  this.price = 7.00;
-
+// prototype function to set name and price
+Order.prototype.getsize = function() { //being called!
+  // this.name not loggingat this point
+  if (this.name = "smallPizza") {
+  this.basePrice = 7.00;
+  return; //working up thru this return
   console.log(this.name);
-  return;
-} else if (this.name === mediumPizza) {
-  this.price = 10.00;
+} else if (this.name = "mediumPizza") {
+  this.basePrice = 10.00;
   $("#sizeDisplayMedium").show();
   console.log(this.name);
   return;
-} else if (this.name === largePizza) {
-  this.price = 15.00;
+} else if (this.name = "largePizza") {
+  this.basePrice = 15.00;
   ("#sizeDisplayLarge").show();
   console.log(this.name);
   return;
@@ -63,22 +43,28 @@ Order.prototype.getsize = function(name) {
 
 // User Interface
 $(document).ready(function() {
-  var order = new Order(1);
+  var order = new Order();
 
   $("#small").click(function() {
     this.name = "smallPizza"; //sets name - works
-    $("#sizeDisplaySmall").show();
+    $("#sizeDisplaySmall").show(); //showsinsidebar
+    order.getsize(); //calls function
+    // this.name console.logs at this point
+  });
+
+  $("#medium").click(function() {
+    this.name = "mediumPizza"; //sets name - works
+    $("#sizeDisplayMedium").show(); //shows in sidebar
     order.getsize();
   });
 
+  $("#large").click(function() {
+    this.name = "largePizza"; //sets name - works
+    $("#sizeDisplayLarge").show(); //shows in sidebar
+    order.getsize();
+  });
 
-
-
-  // $("#chooseSize").submit(function(event) {
-  //   event.preventDefault();
-  //   alert("boo");
-  //   this.name = ($("input[name='size']:checked").val());
-  //   name();
-  //   console.log(this.name);
-  // });
+  // $("#chooseToppings").submit(function(event) {
+  //
+  // })
 });
