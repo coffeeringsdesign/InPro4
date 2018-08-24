bf1dc721394592b3ff356d483126c28c709e1b4d
Kendra <kendra@coffeeringsdesign.com>
2018-08-24 09:37:52 -0700

    create pizza constructor and objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,40 @@
-$(document).ready(function() {
+// Business Logic
+// constructor
+function Pizza(size, price, topping) {
+  this.name = name;
+  this.price = price;
+  this.topping = topping;
+}
 
+// object 1 - assigns price to small
+var size1 = {
+  name: "Small",
+  price: 7.00,
+  topping: []
+};
+
+// object2 - assigns price to medium
+var size2 = {
+  name: "Medium",
+  price: 10.00,
+  topping: []
+};
+
+// object3 - assigns price to large
+var size3 = {
+  name: "Large",
+  price: 15.00,
+  topping: []
+};
+
+Pizza.prototype.topping - function() {
+  var pizzaToppings = this.topping;
+
+};
+
+
+// User Interface
+$(document).ready(function() {
+  $("#")
 
 });
